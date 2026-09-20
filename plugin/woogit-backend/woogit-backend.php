9b27e8de40be04453156c4cf56aa8705d16d1ae6
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-20 15:20:14 +0330

    Remove standalone Bazaar admin settings page

--- plugin/woogit-backend/woogit-backend.php
+++ plugin/woogit-backend/woogit-backend.php
@@ -21,14 +21,12 @@ add_action('admin_menu',static function():void{
     $lifecycle=new \WooGit\Backend\LifecycleAdmin();
     $version=new \WooGit\Backend\VersionAdmin();
     $announcement=new \WooGit\Backend\AnnouncementAdmin();
-    $bazaar=new \WooGit\Backend\BazaarBillingAdmin();
     add_menu_page('WooGit','WooGit','manage_options','woogit',[$accounts,'render'],'dashicons-admin-users',56);
     add_submenu_page('woogit','مدیریت Accounts','Accounts','manage_options','woogit-accounts',[$accounts,'render']);
     add_submenu_page('woogit','WooGit Sessions','Sessionها','manage_options','woogit-sessions',[$lifecycle,'renderSessions']);
     add_submenu_page('woogit','WooGit Trials','Trialها','manage_options','woogit-trials',[$lifecycle,'renderTrials']);
     add_submenu_page('woogit','WooGit App Versions','App Versions','manage_options','woogit-app-versions',[$version,'render']);
     add_submenu_page('woogit','WooGit Announcements','Announcements','manage_options','woogit-announcements',[$announcement,'render']);
-    $bazaar->register();
 },10);
 add_action('admin_head',static function():void{if(($_GET['page']??'')!=='woogit-accounts')return;?>
 <style>
